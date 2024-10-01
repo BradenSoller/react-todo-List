b868b4521bf4583d5f76dff0840f9a6dd466799a
Braden Soller <braden.soller18@gmail.com>
2024-10-01 15:03:29 -0500

    created get route to display todos

--- server/routes/countries.router.js
+++ server/routes/countries.router.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
- 
+    const getTodoList = `SELECT * FROM "todos"`
+    pool.query(getTodoList)
+    .then(result => {
+      console.log("results.rows", result.rows);
+      res.send(result.rows);
+     
+    })
+    .catch((err) => {
+      console.log("ERROR: Get all appointments", err);
+      res.sendStatus(500);
+    });
+  
 })
 
 router.post('/', (req, res) => {
